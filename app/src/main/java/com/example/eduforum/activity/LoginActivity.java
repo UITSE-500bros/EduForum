910cbf77e3232254b0e6569a2da408d8e0c14b0e
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-04-15 16:02:09 +0700

    login activity

---
+++ app/src/main/java/com/example/eduforum/activity/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.eduforum.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.eduforum.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            return insets;
+        });
+    }
+}
