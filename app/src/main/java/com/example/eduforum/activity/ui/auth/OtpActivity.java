4e182ff18b8437e3da17b6ad26fd07bf27bb1438
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-04-20 22:09:19 +0700

    feat:TextChangedListener

--- app/src/main/java/com/example/eduforum/activity/ui/auth/OtpActivity.java
+++ app/src/main/java/com/example/eduforum/activity/ui/auth/OtpActivity.java
@@ -1,6 +1,9 @@
 package com.example.eduforum.activity.ui.auth;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,8 @@ import com.example.eduforum.R;
 
 public class OtpActivity extends AppCompatActivity {
 
+    private EditText inputCode1, inputCode2, inputCode3, inputCode4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +27,90 @@ public class OtpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        inputCode1 = findViewById(R.id.OtpET1);
+        inputCode2 = findViewById(R.id.OtpET2);
+        inputCode3 = findViewById(R.id.OtpET3);
+        inputCode4 = findViewById(R.id.OtpET4);
+        setupOTPInput();
     }
+
+    private void setupOTPInput() {
+        inputCode1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()) {
+                    inputCode2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        inputCode2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()) {
+                    inputCode3.requestFocus();
+                }
+                else {
+                    inputCode1.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        inputCode3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()) {
+                    inputCode4.requestFocus();
+                }
+                else {
+                    inputCode2.requestFocus();
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        inputCode4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().isEmpty()) {
+                    inputCode3.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+    }
+
 }
+
+
