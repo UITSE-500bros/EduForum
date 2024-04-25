f0caa0aa28d52a7d5bcd296fbb8cc5c76383ae7d
Thy Tran <22521461@gm.uit.edu.vn>
2024-04-25 10:31:51 +0700

    refactor: setup firebase analytics and firebase auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -58,6 +59,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-analytics")
+
 //    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 //    implementation("androidx.activity:activity-compose:1.8.2")
 //    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
