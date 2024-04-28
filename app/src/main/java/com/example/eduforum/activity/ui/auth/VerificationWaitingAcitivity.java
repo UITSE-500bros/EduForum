3e498153bf699ff48468cf9d8f2d29020768b78b
Duong Thuan Tri <duongthuantri159@gmail.com>
2024-04-28 17:49:30 +0700

    Refactor: Remove some useless snippet

--- app/src/main/java/com/example/eduforum/activity/ui/auth/VerificationWaitingAcitivity.java
+++ app/src/main/java/com/example/eduforum/activity/ui/auth/VerificationWaitingAcitivity.java
@@ -19,7 +19,6 @@ import com.example.eduforum.R;
 
 public class VerificationWaitingAcitivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityVerificationWaitingAcitivityBinding binding;
 
     @Override
@@ -29,26 +28,8 @@ public class VerificationWaitingAcitivity extends AppCompatActivity {
         binding = ActivityVerificationWaitingAcitivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_verification_waiting_acitivity);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_verification_waiting_acitivity);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 }
