fd68e08b55ed8a38e508fdf1fa235f1d2582d507
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-04-28 15:38:25 +0700

    feat: validation for login

--- app/src/main/java/com/example/eduforum/activity/ui/auth/LoginActivity.java
+++ app/src/main/java/com/example/eduforum/activity/ui/auth/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.eduforum.activity.ui.auth;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +11,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.eduforum.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout emailInput;
+    private TextInputLayout passwordInput;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,28 @@ public class LoginActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             return insets;
         });
+
+        emailInput=findViewById(R.id.TIL_Email);
+        passwordInput=findViewById(R.id.TIL_Password);
+        loginButton=findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Objects.requireNonNull(emailInput.getEditText()).getText().toString().isEmpty()){
+                    emailInput.setError("Email is required");
+                    return;
+                }
+
+                if(Objects.requireNonNull(passwordInput.getEditText()).getText().toString().isEmpty()){
+                    passwordInput.setError("Password is required");
+                    return;
+                }
+
+            }
+        });
+
+
     }
+
 }
