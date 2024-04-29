b3e4b66afbfc85a5a0562d07027b61f88b256e8f
Thy Tran <22521461@gm.uit.edu.vn>
2024-04-29 12:19:08 +0700

    feat: navigate to welcome when first time open app

--- app/src/main/java/com/example/eduforum/activity/ui/auth/LoginActivity.java
+++ app/src/main/java/com/example/eduforum/activity/ui/auth/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.eduforum.activity.ui.auth;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.eduforum.R;
 import com.example.eduforum.activity.ui.main.MainActivity;
+import com.example.eduforum.activity.ui.welcome.WelcomeActivity;
 import com.example.eduforum.activity.viewmodel.auth.LoginViewModel;
 import com.example.eduforum.databinding.ActivityLoginBinding;
 import com.google.android.material.snackbar.Snackbar;
@@ -28,15 +30,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel viewModel;
     private ActivityLoginBinding binding;
+    private SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
         viewModel.getSignedInUser().observe(this, this::updateUI);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        prefs = getSharedPreferences("com.example.eduforum", MODE_PRIVATE);
+        if (prefs.getBoolean("firstRun", true)) {
+            // Do first run stuff here then set 'firstrun' as false
+            // using the following line to edit/commit prefs
+            Intent i = new Intent(this, WelcomeActivity.class);
+            startActivity(i);
+            prefs.edit().putBoolean("firstRun", false).apply();
+            finish();
+
+        }
+
 
         binding.setViewModel(viewModel);
         binding.setLifecycleOwner(this);
