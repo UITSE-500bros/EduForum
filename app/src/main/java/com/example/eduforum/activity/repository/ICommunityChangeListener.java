975404b63f55c561086d3f055b80c7866c28d4f7
Thy Tran <22521461@gm.uit.edu.vn>
2024-04-30 17:08:21 +0700

    fix: community repo

--- app/src/main/java/com/example/eduforum/activity/repository/ICommunityChangeListener.java
+++ app/src/main/java/com/example/eduforum/activity/repository/ICommunityChangeListener.java
@@ -2,7 +2,7 @@ package com.example.eduforum.activity.repository;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 
-public interface CommunityChangeListener {
+public interface ICommunityChangeListener {
     void onDocumentChange(DocumentSnapshot snapshot);
     void onError(Exception exception);
     void onDocumentNotFound();
