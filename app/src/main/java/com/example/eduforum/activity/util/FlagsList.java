2b337fe2aeb06dd3e20d5ce5006983e1aed725ec
DPhngNam <22520908@gm.uit.edu.vn>
2024-04-30 17:16:14 +0700

    feat: fix error

--- app/src/main/java/com/example/eduforum/activity/util/FlagsList.java
+++ app/src/main/java/com/example/eduforum/activity/util/FlagsList.java
@@ -1,6 +1,8 @@
 package com.example.eduforum.activity.util;
 
 public class FlagsList {
+    public static final String ERROR_REGISTER = "ERROR_REGISTER";
+    public static final String ERROR_REGISTER_EMAIL_EXISTED = "ERROR_REGISTER_EMAIL_EXISTED";
     public static String DEBUG_REGISTER_FLAG = "DEBUG_REGISTER";
     public static String DEBUG_LOGIN_FLAG = "DEBUG_LOGIN";
     public static String DEBUG_TOPIC_FLAG = "DEBUG_TOPIC";
@@ -8,10 +10,9 @@ public class FlagsList {
     public static String ERROR_LOGIN_WRONG_CREDENTIALS = "ERROR_LOGIN_WRONG_CREDENTIALS";
     public static String ERROR_COMMUNITY_FAILED_TO_CREATE = "ERROR_COMMUNITY_FAILED_TO_CREATE";
     public static String ERROR_COMMUNITY_CODE_NOT_EXIST = "ERROR_COMMUNITY_CODE_NOT_EXIST";
-    public static String ERROR_COMMUNITY_ADD_USER = "ERROR_COMMUNITY_ADD_USER"
+    public static String ERROR_COMMUNITY_ADD_USER = "ERROR_COMMUNITY_ADD_USER";
     public static int CONNECTION_RETRIES = 3;
     public static String APPLICATION_ENVIRONMENT = "production";
-
 
     public static String DEBUG_COMMUNITY_FLAG = "DEBUG_COMMUNITY";
 }
