5639e5e5b6525ded78911c09f9a6c67784aae1c6
DPhngNam <22520908@gm.uit.edu.vn>
2024-05-06 15:40:00 +0700

    fix: seperate files

---
+++ app/src/main/java/com/example/eduforum/activity/model/PostModel/Category.java
@@ -0,0 +1,40 @@
+package com.example.eduforum.activity.model.PostModel;
+
+public class Category {
+    public String categoryID;
+    public String title;
+    public Boolean isAnnouncement;
+
+    public Category() {
+    }
+
+    public Category(String categoryID, String title, Boolean isAnnouncement) {
+        this.categoryID = categoryID;
+        this.title = title;
+        this.isAnnouncement = isAnnouncement;
+    }
+
+    public String getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(String categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getAnnouncement() {
+        return isAnnouncement;
+    }
+
+    public void setAnnouncement(Boolean announcement) {
+        isAnnouncement = announcement;
+    }
+}
