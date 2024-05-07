b7f6edabdbe5f729b8d190046cf6d74014d340e3
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-05-07 22:39:00 +0700

    refactor: delete forumacivity

--- app/src/main/java/com/example/eduforum/activity/ui/auth/CommunityActivity.java
+++ app/src/main/java/com/example/eduforum/activity/ui/auth/CommunityActivity.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.eduforum.R;
 
-public class PostActivity extends AppCompatActivity {
+public class CommunityActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_post);
+        setContentView(R.layout.activity_community);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
