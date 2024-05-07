8989b8be8e2b07d87789917175b99890c6c1cd3e
Nguyen Trinh Vinh Thinh <thinhss123321@gmail.com>
2024-05-08 00:48:47 +0700

    Feat: add new editText including redoButton, undoButton, boldButton, ...

--- app/src/main/java/com/example/eduforum/activity/ui/auth/CreatePostActivity.java
+++ app/src/main/java/com/example/eduforum/activity/ui/auth/CreatePostActivity.java
@@ -1,9 +1,13 @@
 package com.example.eduforum.activity.ui.auth;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -30,12 +34,53 @@ public class CreatePostActivity extends AppCompatActivity {
             return insets;
         });
 
+        //Stlye rich editor through binding
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_post);
         binding.contentRichEditor.setEditorHeight(217);
         binding.contentRichEditor.setEditorFontSize(14);
         binding.contentRichEditor.setEditorFontSize(R.color.black);
         binding.contentRichEditor.setEditorBackgroundColor(R.color.white);
-
         binding.contentRichEditor.setPlaceholder("Nhập nội dung bài viết");
+
+        //set up action buttons
+        binding.actionRedo.setOnClickListener(v -> binding.contentRichEditor.redo());
+        binding.actionUndo.setOnClickListener(v -> binding.contentRichEditor.undo());
+        binding.actionBold.setOnClickListener(v -> binding.contentRichEditor.setBold());
+        binding.actionItalic.setOnClickListener(v -> binding.contentRichEditor.setItalic());
+        binding.actionUnderline.setOnClickListener(v -> binding.contentRichEditor.setUnderline());
+        binding.actionAlignCenter.setOnClickListener(v -> binding.contentRichEditor.setAlignCenter());
+        binding.actionAlignLeft.setOnClickListener(v -> binding.contentRichEditor.setAlignLeft());
+        binding.actionAlignRight.setOnClickListener(v -> binding.contentRichEditor.setAlignRight());
+        binding.actionHeading1.setOnClickListener(v -> binding.contentRichEditor.setHeading(1));
+        binding.actionHeading2.setOnClickListener(v -> binding.contentRichEditor.setHeading(2));
+        binding.actionHeading3.setOnClickListener(v -> binding.contentRichEditor.setHeading(3));
+        binding.actionInsertBullets.setOnClickListener(v -> binding.contentRichEditor.setBullets());
+        binding.actionInsertNumbers.setOnClickListener(v -> binding.contentRichEditor.setNumbers());
+        binding.actionInsertLink.setOnClickListener(v -> {
+            AlertDialog builder = new AlertDialog.Builder(this).create();
+            builder.setTitle("Chèn link vào bài viết");
+
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+
+            final EditText textInput = new EditText(this);
+            textInput.setHint("Text");
+            layout.addView(textInput);
+            final EditText urlInput = new EditText(this);
+            urlInput.setHint("URL");
+            layout.addView(urlInput);
+
+            builder.setView(layout);
+
+            builder.setButton(DialogInterface.BUTTON_POSITIVE, "OK", (dialog, which) -> {
+                String text = textInput.getText().toString();
+                String url = urlInput.getText().toString();
+                binding.contentRichEditor.insertLink(url, text);
+            });
+
+            builder.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", (dialog, which) -> dialog.cancel());
+
+            builder.show();
+        });
     }
 }
