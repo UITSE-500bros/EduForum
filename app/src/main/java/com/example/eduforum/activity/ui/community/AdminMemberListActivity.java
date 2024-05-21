d03326b107458e81ccd0bdc44d39a9f840d263a4
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-05-21 18:14:06 +0700

    feat: member list

--- app/src/main/java/com/example/eduforum/activity/ui/community/AdminMemberListActivity.java
+++ app/src/main/java/com/example/eduforum/activity/ui/community/AdminMemberListActivity.java
@@ -1,6 +1,7 @@
 package com.example.eduforum.activity.ui.community;
 
 import android.os.Bundle;
+import android.view.MenuInflater;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +10,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.eduforum.R;
+import com.example.eduforum.databinding.ActivityAdminMemberListBinding;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class AdminMemberListActivity extends AppCompatActivity {
+    private ActivityAdminMemberListBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_member_list);
+        binding = ActivityAdminMemberListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // This should be the only call to setContentView
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        MaterialToolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getMenuInflater().inflate(R.menu.search_menu, toolbar.getMenu());
+
+
+
     }
 }
