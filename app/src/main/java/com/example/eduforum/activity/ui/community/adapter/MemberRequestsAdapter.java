f82d134034a8ab19546f357dcd66eba7faddffca
Duong Thuan Tri <duongthuantri159@gmail.com>
2024-05-25 16:24:57 +0700

    Feat: Upload profilePicture of a user in MemberRequest and Everyone screens

--- app/src/main/java/com/example/eduforum/activity/ui/community/adapter/MemberRequestsAdapter.java
+++ app/src/main/java/com/example/eduforum/activity/ui/community/adapter/MemberRequestsAdapter.java
@@ -7,8 +7,11 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.eduforum.activity.model.user_manage.User;
 import com.example.eduforum.databinding.ItemMemberRequestBinding;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.List;
 
@@ -58,7 +61,6 @@ public class MemberRequestsAdapter extends RecyclerView.Adapter<MemberRequestsAd
             User user = memberRequests.get(position);
             binding.userNameTextView.setText(user.getName());
             binding.khoaTextView.setText(user.getDepartment());
-            // user avt
 
             // buttons action
             binding.acceptRequestButton.setOnClickListener(v -> {
@@ -71,6 +73,12 @@ public class MemberRequestsAdapter extends RecyclerView.Adapter<MemberRequestsAd
                     listener.onReview(user, false);
                 }
             });
+            if(memberRequests.get(position).getProfilePicture()!=null){
+                StorageReference storageReference = FirebaseStorage.getInstance().getReference(memberRequests.get(position).getProfilePicture());
+                Glide.with(binding.getRoot().getContext())
+                        .load(storageReference)
+                        .into(binding.memberRequestAva);
+            }
 
         }
     }
