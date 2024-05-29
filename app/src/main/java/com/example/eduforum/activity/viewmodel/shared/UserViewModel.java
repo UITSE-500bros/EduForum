06ac551e79913c561d5f9b2982907a7112903098
Thy Tran <22521461@gm.uit.edu.vn>
2024-05-29 14:23:00 +0700

    feat: update user's profile

--- app/src/main/java/com/example/eduforum/activity/viewmodel/shared/UserViewModel.java
+++ app/src/main/java/com/example/eduforum/activity/viewmodel/shared/UserViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.eduforum.activity.model.user_manage.User;
+import com.example.eduforum.activity.repository.user.IUpdate;
 import com.example.eduforum.activity.repository.user.IUserCallback;
 import com.example.eduforum.activity.repository.user.UserRepository;
 
@@ -22,6 +23,17 @@ public class UserViewModel extends ViewModel {
 
     public void setCurrentUserLiveData(User user) {
         currentUser.setValue(user);
+    }
+    public void updateProfile(User user) {
+        userRepository.updateProfile(user, new IUpdate() {
+            @Override
+            public void onUpdateSuccess(User newUser) {
+                currentUser.setValue(newUser);
+            }
+            @Override
+            public void onUpdateFailed(String message) {
+            }
+        });
     }
 
     public void getCurrentUser(IUserCallback callback) {
