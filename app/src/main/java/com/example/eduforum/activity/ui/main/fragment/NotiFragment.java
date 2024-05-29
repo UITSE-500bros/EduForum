b5d194bfa19989b5a8fa8df38d47f0ec9c95b60d
DPhngNam <22520908@gm.uit.edu.vn>
2024-05-29 21:36:26 +0700

    fix: UI list noti

--- app/src/main/java/com/example/eduforum/activity/ui/main/fragment/NotiFragment.java
+++ app/src/main/java/com/example/eduforum/activity/ui/main/fragment/NotiFragment.java
@@ -35,44 +35,21 @@ public class NotiFragment extends Fragment {
     private UserViewModel userViewModel;
     private NotificationAdapter adapter;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public NotiFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotiFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    
     public static NotiFragment newInstance(String param1, String param2) {
         NotiFragment fragment = new NotiFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -102,16 +79,12 @@ public class NotiFragment extends Fragment {
         });
         viewModel.getNotificationList();
 
-        RecyclerView recyclerView = binding.notiRecyclerView;
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.notiRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.notiRecyclerView.setAdapter(adapter);
 
         binding.readAllButton.setOnClickListener(v -> {
             viewModel.markAllAsRead();
-
         });
     }
-
-
 
 }
