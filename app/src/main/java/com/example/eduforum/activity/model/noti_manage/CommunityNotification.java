b9f1ac821bc12d16194b775463a90f80c2aedd7d
Thy Tran <22521461@gm.uit.edu.vn>
2024-05-30 19:47:21 +0700

    fix: lag

--- app/src/main/java/com/example/eduforum/activity/model/noti_manage/CommunityNotification.java
+++ app/src/main/java/com/example/eduforum/activity/model/noti_manage/CommunityNotification.java
@@ -1,2 +1,24 @@
-package com.example.eduforum.activity.model.noti_manage;public class CommunityNotification {
+package com.example.eduforum.activity.model.noti_manage;
+
+public class CommunityNotification {
+    private String communityID;
+    private String name;
+
+    public CommunityNotification() {}
+
+    public String getCommunityID() {
+        return communityID;
+    }
+
+    public void setCommunityID(String communityID) {
+        this.communityID = communityID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
