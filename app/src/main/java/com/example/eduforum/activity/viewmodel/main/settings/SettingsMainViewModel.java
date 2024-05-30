140a5b6414978d9d6843514c3711c9b0493cc2fa
Duong Thuan Tri <duongthuantri159@gmail.com>
2024-05-30 12:28:56 +0700

    Feat: Signout

--- app/src/main/java/com/example/eduforum/activity/viewmodel/main/settings/SettingsMainViewModel.java
+++ app/src/main/java/com/example/eduforum/activity/viewmodel/main/settings/SettingsMainViewModel.java
@@ -2,5 +2,14 @@ package com.example.eduforum.activity.viewmodel.main.settings;
 
 import androidx.lifecycle.ViewModel;
 
+import com.example.eduforum.activity.repository.user.UserRepository;
+
 public class SettingsMainViewModel extends ViewModel {
+    UserRepository userRepository;
+    public SettingsMainViewModel(){
+        userRepository = UserRepository.getInstance();
+    }
+    public void signOut() {
+        userRepository.signOut();
+    }
 }
